013423622c5df4178e0db951913cdec3d69b7879
Jthapa-1 <141063951+Jthapa-1@users.noreply.github.com>
2023-08-02 12:10:44 -0400

    error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,15 +34,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
 #added text box
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    streamlit.write('The user entered ', fruit_choice)
+    #streamlit.write('The user entered ', fruit_choice)
       
-    if fruit_choice :
+    if not fruit_choice :
         streamlit.error('Please select a fruit to get information.')
     else:    
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
         #normalizing json   
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
-    
         #displayed normalized json  
         streamlit.dataframe(fruityvice_normalized)
 
