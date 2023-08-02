cd40e84163acd9d1bbe2b7a630b0dda228ee3133
Jthapa-1 <141063951+Jthapa-1@users.noreply.github.com>
2023-08-02 12:05:40 -0400

    Added Try except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,15 +30,24 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruitvice api
 streamlit.header("Fruityvice Fruit Advice!")
-#added text box
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#normalizing json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#displayed normalized json 
-streamlit.dataframe(fruityvice_normalized)
+try:
+#added text box
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    streamlit.write('The user entered ', fruit_choice)
+      
+    if fruit_choice :
+        streamlit.error('Please select a fruit to get information.')
+    else:    
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        #normalizing json   
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())   
+    
+        #displayed normalized json  
+        streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error()
 
 streamlit.stop()
 
