e0caa331384157f206842964a4cfa733a3b6a274
Jthapa-1 <141063951+Jthapa-1@users.noreply.github.com>
2023-08-02 13:42:52 -0400

    added insert function

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ except URLError as e:
     streamlit.error()
 
 
-
 #snow flake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -56,6 +55,19 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows=get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into FRUIT_LOAD_LIST values('Streamlit')")
+        return "Thanks for adding "+new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like Add?')
+if streamlit.button('Add a fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function=insert_row_snowflake(add_my_fruit)
+    streamlit.dataframe(back_from_function)
+
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
